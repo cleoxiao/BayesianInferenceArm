b12510ddac1b2bafa81a788dec33c4d5eb83a0d3
Difeng YU <difeng.yu@outlook.com>
2026-06-16 16:17:40 +0100

    Update batches_simple_reaching_task_baseline.py

--- batches/batches_simple_reaching_task_baseline.py
+++ batches/batches_simple_reaching_task_baseline.py
@@ -18,7 +18,7 @@ param_grid = {
     "p_shoulder_init": [np.array([0.2, -0.2])],
     "self_terminate": [True],
     "r_target": [0.005],
-    "n_runs": [10],
+    "n_runs": [1],
     "n_trials": [1],
     "visual_feedback": [True],              # cursor always visible
     "visual_intervention_bool": [False],    # no spatial distortion
